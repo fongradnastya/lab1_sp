9f9d3e90b3c773d6a85df93ca9db2521f9f3fb7d
Student <stud@Yenisei.home>
2023-02-08 21:54:00 +0700

    Started matrix input

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
 
 
 char* GetString(int *len) {
@@ -23,39 +25,84 @@ char* GetString(int *len) {
     }
 
     s[*len] = '\0'; // завершаем строку символом конца строки
+    return s;
 }
 
 void PrintMatrix(int ** matrix, int size_x, int size_y){
-    for(int i = 0; i < size_y; i++){
-        for(int j = 0; j < size_x - 1; j++){
+    for(int i = 0; i < size_x; i++){
+        for(int j = 0; j < size_y - 1; j++){
             printf("%d, ", matrix[i][j]);
         }
         printf("%d\n", matrix[i][-1]);
     }
 }
 
-void CreateMatrix(int a, int b){
+int** CreateMatrix(int a, int b){
     int** matrix = (int**) malloc(a * sizeof(int*));
     for(int i = 0; i < a; i++){
         matrix[i] = (int*)malloc(b * sizeof(int));
     }
-    PrintMatrix(matrix, a, b);
+    return matrix;
 }
 
-void InputMatrix(int size_x, int size_y){
+int InputMatrix(int** matrix, int size_x, int size_y){
+    printf("Please enter a %d:%d matrix", size_x, size_y);
+    for(int i = 0; i < size_x; i++){
+        printf("%d string: ", i + 1);
+        int num;
+        for(int j = 0; j < size_y; j++){
+            int res = scanf("%d", &num);
+            if(res == 1){
+                matrix[i][j] = num;
+            }
+            else return 0;
+        }
+    }
+    return 1;
+}
 
+int InputNumber(int* val){
+    int len;
+    char* str = GetString(&len);
+    printf("Str %s\n", str);
+    for(int i = 0; i < strlen(str); i++)
+    {
+        if (!isdigit(str[i]) && !(i == 0 && str[0] == '-')) return 0;
+    }
+    *val = atoi(str);
+    return 1;
+}
+
+void InputSize(int* size){
+    int x_size = 0;
+    while(x_size < 1) {
+        printf("Please, enter x-size: ");
+        InputNumber(&x_size);
+    }
+    int y_size = 0;
+    while(y_size < 1) {
+        printf("Please, enter y-size: ");
+        InputNumber(&y_size);
+    }
+    size[0] = x_size;
+    size[1] = y_size;
 }
 
 int main() {
-    int res = 0;
-    int a;
-    char* str = " ";
-    while(str){
-        setbuf(stdin, NULL);
-        printf("Please, enter a matrix size: ");
-        
-        res = scanf("%d%s", &a, str);
+    int size [] = {0, 0};
+    InputSize(size);
+    printf("Answer %d %d\n", size[0], size[1]);
+    int** matrix = (int**) malloc(size[0] * sizeof(int*));
+    for(int i = 0; i < size[0]; i++){
+        matrix[i] = (int*)malloc(size[1] * sizeof(int));
     }
-    CreateMatrix(a, a);
+    int res = InputMatrix(matrix, size[0], size[1]);
+    if(res){
+        PrintMatrix(matrix, size[0], size[1]);
+    }
+    for(int i = 0; i < size[0]; i++){
+        free(matrix[i]);
+    }
+    free(matrix);
     return 0;
 }
